a3b03540abcb07883c8ceeb6dd534b56edea9ef8
taylulz <taylulzcode@gmail.com>
2021-02-12 11:25:51 -0800

    make room for description property

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -5,21 +5,24 @@ const masterKegList = [
   {
     name: 'Goose Neck Pilsner',
     brewery: 'Pillsbury Brewery',
-    abv: '6.7',
+    abv: 6.7,
+    description: 'has a pill-y, light flavor, great for the whole family',
     price: 10,
     pints: 124
   },
   {
     name: 'Chucks Brown Ale',
     brewery: 'Hilltop',
-    abv: '8',
+    abv: 8,
+    description: 'Chunky, dark, malty flaves',
     price: 15,
     pints: 124
   },
   {
     name: 'PNW IPA',
     brewery: 'Mountain Water',
-    abv: '8.8',
+    abv: 8.8,
+    description: 'Like sipping from a river',
     price: 14,
     pints: 124
   }
@@ -33,6 +36,7 @@ export default function KegList(){
           name={keg.name}
           brewery={keg.brewery}
           abv={keg.abv}
+          description={keg.description}
           price={keg.price}
           pints={keg.pints}
           key={index} 
