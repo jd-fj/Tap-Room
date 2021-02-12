5d6d45cc5f8ef1d3e40a708b58fe14934b0c503f
taylulz <taylulzcode@gmail.com>
2021-02-12 09:27:44 -0800

    pass props from KegList to Keg component

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function Keg(){
+export default function Keg(props){
   return (
     <>
-      <h2>Keg Name</h2>
-      <h4>brewery:</h4>
-      <h4>ABV:</h4>
-      <h4>price:</h4>
+      <h2>{props.name}</h2>
+      <h4>Brewery: {props.brewery}</h4>
+      <h4>ABV: {props.abv}</h4>
+      <h4>price: {props.price}</h4>
       <hr/>
     </>
   )
--- src/components/KegList.js
+++ src/components/KegList.js
@@ -3,6 +3,17 @@ import Keg from './Keg'
 
 export default function KegList(){
   return(
-    <Keg />
+    <>
+      <Keg 
+      name='Goose Neck Pilsner'
+      brewery='Pillsbury Brewery'
+      abv='6.7'
+      price='10'/>
+      <Keg 
+      name='Chucks Brown Ale'
+      brewery='Hilltop'
+      abv='8'
+      price='15'/>
+    </>
   );
 }
