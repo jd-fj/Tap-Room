4a1362f5ca5ac2ed2c1d7685be8fab08b4348ca2
taylulz <taylulzcode@gmail.com>
2021-02-12 14:40:21 -0800

    correctly display bg img using inline css object

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+// import './App.css';
 import Header from "./Header";
 import KegControl from './KegControl';
+import React from 'react';
+import fizz from '../img/fizz.jpeg';
 
-export default function App() {
+export default function App(props) {
+  const appStyles = {
+    backgroundImage: `url("${fizz}")`
+  }
   return (
-    <>
+    <div style={appStyles}>
     <Header/>
     <KegControl/>
-    </>
+    </div>
   );
 }
