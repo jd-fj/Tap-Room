0e6583142cc6b15058954c255b061add37975c69
taylulz <taylulzcode@gmail.com>
2021-02-12 10:04:28 -0800

    add static masterKegList and .map it

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,21 +1,43 @@
 import React from 'react';
-import Keg from './Keg'
+import Keg from './Keg';
+
+const masterKegList = [
+  {
+    name: 'Goose Neck Pilsner',
+    brewery: 'Pillsbury Brewery',
+    abv: '6.7',
+    price: 10,
+    pints: 124
+  },
+  {
+    name: 'Chucks Brown Ale',
+    brewery: 'Hilltop',
+    abv: '8',
+    price: 15,
+    pints: 124
+  },
+  {
+    name: 'PNW IPA',
+    brewery: 'Mountain Water',
+    abv: '8.8',
+    price: 14,
+    pints: 124
+  }
+];
 
 export default function KegList(){
   return(
     <>
-      <Keg 
-      name='Goose Neck Pilsner'
-      brewery='Pillsbury Brewery'
-      abv='6.7'
-      price='10'
-      pints='124'/>
-      <Keg 
-      name='Chucks Brown Ale'
-      brewery='Hilltop'
-      abv='8'
-      price='15'
-      pints='124'/>
+      {masterKegList.map((keg, index) => 
+        <Keg 
+          name={keg.name}
+          brewery={keg.brewery}
+          abv={keg.abv}
+          price={keg.price}
+          pints={keg.pints}
+          key={index} 
+          />
+      )}
     </>
   );
 }
