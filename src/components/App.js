29a2976b0b5994aa0b8c74e4f43af3dbf3ba656b
taylulz <taylulzcode@gmail.com>
2021-02-15 07:49:05 -0800

    correctly implement bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,19 @@
+import {Container, Row, Col, Jumbotron} from 'react-bootstrap';
 // import './App.css';
 import Header from "./Header";
 import KegControl from './KegControl';
 import React from 'react';
-import fizz2 from '../img/fizz2.jpeg';
 
-export default function App(props) {
-  const appStyles = {
-    backgroundImage: `url("${fizz2}")`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover'
-  }
+export default function App() {
 
   return (
     <>
-      <Header/>
-      <div style={appStyles}>
+      <Jumbotron>
+        <Header/>
+      </Jumbotron>
+      <Container>
         <KegControl/>
-      </div>
+      </Container>
     </>
   );
 }
